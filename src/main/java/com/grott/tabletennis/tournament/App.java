04f94b6f2bc82d07a853a892c4e5e8bcff8abbad
Tobias Grott <tobiasgrott@gmail.com>
2016-12-06 19:19:47 +0100

    fixed unused imports

--- src/main/java/com/grott/tabletennis/tournament/App.java
+++ src/main/java/com/grott/tabletennis/tournament/App.java
@@ -3,7 +3,6 @@ package com.grott.tabletennis.tournament;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
